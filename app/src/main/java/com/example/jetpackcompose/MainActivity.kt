f1fd1a1ce36530df71ef3ed11ed79c9d8e6551e4
SARA-amrani <172842309+SARA-amrani@users.noreply.github.com>
2025-03-16 12:18:06 +0000

    Paramétrage de l’apparence

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -3,15 +3,10 @@ package com.example.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,26 +25,14 @@ class MainActivity : ComponentActivity() {
         }   */
 
         setContent {
-            Text("Bonjour tout le monde !")
+            Text(
+                text = "Bonjour tout le monde !",
+                fontWeight = FontWeight.Bold,
+                fontSize = 32.sp,
+                color = Color.Magenta)
         }
         //setContent(null,{Text("OK")})
 
     }
 
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetpackComposeTheme {
-        Greeting("Android")
-    }
 }
